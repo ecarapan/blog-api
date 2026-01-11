484e57c3c9e9896105f066b1f74b348730a10bf0
ecarapan <92954426+ecarapan@users.noreply.github.com>
2026-01-11 01:00:53 -0700

    Implement user page

--- frontend/src/pages/userPage/UserPage.jsx
+++ frontend/src/pages/userPage/UserPage.jsx
@@ -3,23 +3,45 @@ import { useUser } from "@/hooks/useUser";
 import { useParams } from "react-router";
 import { usePosts } from "@/hooks/usePosts";
 import { Post } from "@/pages/homePage/post/Post.jsx";
+import { useState, useEffect } from "react";
 
 export function UserPage() {
   const { userId } = useParams();
   const { user, userLoading, userError } = useUser(userId);
-  const { postsList, loading, error } = usePosts();
 
-  if (userError || error) {
+  const [userPostsList, setUserPostsList] = useState([]);
+  const [userPostsLoading, setUserPostsLoading] = useState(true);
+  const [userPostsError, setUserPostsError] = useState(null);
+
+  useEffect(() => {
+    async function fetchUserPosts() {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/users/${userId}/posts`
+        );
+        const userPostsData = await response.json();
+        setUserPostsList(userPostsData);
+        setUserPostsLoading(false);
+      } catch (err) {
+        setUserPostsError(err.message || "Error fetching posts");
+        setUserPostsLoading(false);
+      }
+    }
+
+    fetchUserPosts();
+  }, [userId]);
+
+  if (userError || userPostsError) {
     return (
       <div className={`${styles.homePage} ${styles.loading}`}>
         <h1 role="alert" aria-live="assertive" className={styles.errorSpinner}>
-          {userError || error}
+          {userError || userPostsError}
         </h1>
       </div>
     );
   }
 
-  if (userLoading || loading) {
+  if (userLoading || userPostsLoading) {
     return (
       <div className={`${styles.homePage} ${styles.loading}`}>
         <div role="status" aria-live="polite" className={styles.loadingSpinner}>
@@ -34,7 +56,7 @@ export function UserPage() {
       <h1>Profile</h1>
       <p>{user.name}</p>
       <p>{user.email}</p>
-      {postsList.map((post) => (
+      {userPostsList.map((post) => (
         <Post
           key={post.id}
           id={post.id}
