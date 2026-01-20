d2c6324846c2ca45811a9f0b4837c7147a85df63
ecarapan <92954426+ecarapan@users.noreply.github.com>
2026-01-19 22:47:14 -0700

    Add selected style to navbar

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -1,32 +1,34 @@
 import styles from "@/components/sidebar/Sidebar.module.css";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import homeIcon from "@/assets/home-variant-outline.svg";
 
 export function Sidebar({ isOpen }: { isOpen: boolean }) {
+  const location = useLocation();
+
   return (
     <aside className={`${styles.sidebar} ${!isOpen ? styles.closed : ""}`}>
       <ul>
-        <li>
+        <li className={location.pathname === "/" ? styles.selected : ""}>
           <img src={homeIcon} />
-          <Link to="/" className={styles.homeLink}>
+          <Link to="/" className={styles.link}>
             Home
           </Link>
         </li>
         <li>
           <img src={homeIcon} />
-          <Link to="/" className={styles.homeLink}>
+          <Link to="/" className={styles.link}>
             Messages
           </Link>
         </li>
         <li>
           <img src={homeIcon} />
-          <Link to="/" className={styles.homeLink}>
+          <Link to="/" className={styles.link}>
             Popular
           </Link>
         </li>
         <li>
           <img src={homeIcon} />
-          <Link to="/" className={styles.homeLink}>
+          <Link to="/" className={styles.link}>
             Oldest
           </Link>
         </li>
